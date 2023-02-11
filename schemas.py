c676f8d52211f3a5b2351ceb74a8555a24afd5b5
Caleb Yeboah <calebyeboah27@gmail.com>
2023-02-11 14:53:20 +0000

    Added schemas for ArticleBase and Article Display

--- schemas.py
+++ schemas.py
@@ -1,4 +1,15 @@
+from typing import List
 from pydantic import BaseModel
+
+
+# Article inside  UserDisplay
+class Article(BaseModel):
+    title: str
+    content: str
+    published: bool
+
+    class Config:
+        orm_mode = True
 
 
 class UserBase(BaseModel):
@@ -10,5 +21,30 @@ class UserBase(BaseModel):
 class UserDisplay(BaseModel):
     username: str
     email: str
+    articles: List[Article] = []
+    class Config:
+        orm_mode = True
+
+
+# User inside Article Display
+class User(BaseModel):
+    id: int
+    username: str
+    class Config:
+        orm_mode = True
+
+
+class ArticleBase(BaseModel):
+    title: str
+    content: str
+    published: bool
+    creator_id: int
+
+
+class ArticleDisplay(BaseModel):
+    title: str
+    content: str
+    published: bool
+    user: User
     class Config:
         orm_mode = True
