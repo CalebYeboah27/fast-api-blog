7b9ea08dd388a4b58f60c248a0471c3dddc72358
Caleb Yeboah <calebyeboah27@gmail.com>
2023-02-11 15:34:31 +0000

    Added aricle router to the application

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import Depends, FastAPI
-from router import blog_get, blog_post, user
+from router import blog_get, blog_post, user, article
 from db import models
 from db.database import engine, get_db
 
 
 app = FastAPI()
 app.include_router(user.router)
+app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
